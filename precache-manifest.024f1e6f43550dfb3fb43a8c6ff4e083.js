34c61fa2e65972302db3b68833c4030734375828
my-dev-git <siva.theme@gmail.com>
2019-08-16 15:35:15 +0530

    first commit

---
+++ precache-manifest.024f1e6f43550dfb3fb43a8c6ff4e083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fa633578204a51f97875c145976cc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9c4b26e1ba0c9b7e1e2",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ecdd84104fa33b673edb",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "f9c4b26e1ba0c9b7e1e2",
+    "url": "/static/js/2.ca090081.chunk.js"
+  },
+  {
+    "revision": "ecdd84104fa33b673edb",
+    "url": "/static/js/main.8ee724d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
